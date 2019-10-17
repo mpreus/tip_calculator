3cf6d58118a91fd0ea08b7ffe675e55176aad2a6
Maciej Preus <mpreus@onet.eu>
2019-10-17 14:17:03 +0200

    Add JS app for calculating tip

---
+++ app/app.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded", init);
+function init() {
+	
+	let output = document.querySelector("div.output");
+	let costInput = document.querySelector("input");
+
+	let veryLowTip = document.getElementById("veryLowTip");
+	veryLowTip.addEventListener("click", function() {
+		let vltInput = costInput.value;
+		if (vltInput === '') {
+			output.innerText = 'Try to enter your sum first';
+		}
+		else {
+			let vltTipValue = (vltInput * 0.1).toFixed(2);
+			
+			if (vltInput < 20) {
+				output.innerText = "Even if your bill is below 20 PLN, the tip lower then 2 PLN seems controversial";
+			}
+			else {
+				output.innerText = `For ${vltInput} PLN bill, suggested value of the tip is ${vltTipValue} PLN`;
+			}
+			costInput.value = "";
+		}
+	});
+
+	let moderateTip = document.getElementById("moderateTip");
+	moderateTip.addEventListener("click", function() {
+		let mtInput = costInput.value;
+		if (mtInput === '') {
+			output.innerText = 'Try to enter your sum first';
+		}
+		else {
+			let mtTipValue = (mtInput * 0.15).toFixed(2);
+			if (mtInput < 14) {
+				output.innerText = "Even if your bill is below 14 PLN, the tip lower then 2 PLN seems controversial";
+			}
+			else {
+				output.innerText = `For ${mtInput} PLN bill, suggested value of the tip is ${mtTipValue} PLN`;
+			}
+			costInput.value = "";
+		}
+	});
+
+	let highTip = document.getElementById("highTip");
+	highTip.addEventListener("click", function() {
+		let hInput = costInput.value;
+		if (hInput === '') {
+			output.innerText = 'Try to enter your sum first';
+		}
+		else {
+			let hTipValue = (hInput * 0.2).toFixed(2);
+			if (hInput < 10) {
+				output.innerText = "Even if your bill is below 10 PLN, the tip lower then 2 PLN seems controversial";
+			}
+			else {
+				output.innerText = `For ${hInput} PLN bill, suggested value of the tip is ${hTipValue} PLN`;
+			}
+			costInput.value = "";
+		}
+	});
+
+}
